34483fdf1b6a207a0a2c4718d4aea287ff2ddb56
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-04-02 21:04:28 +0200

    add full-upgrade function

--- bashrc.sh
+++ bashrc.sh
@@ -51,6 +51,32 @@ win-reboot() {
     sudo reboot
 }
 
+full-upgrade() {
+    sudo pacman -Syu --noconfirm
+
+    sudo fwupdmgr update -y --no-reboot-check
+
+    yay -Syu \
+        --noconfirm \
+        --answerdiff None \
+        --answerclean None \
+        --removemake \
+        --ignore-outofdate
+
+    flatpak update -y
+
+    rm -rf ~/.cache/*
+    read -r -p "[Y/n] recommended reboot. reboot now?" response
+    case "$response" in
+    [nN]) 
+        return
+        ;;
+    *)
+        sudo systemctl reboot
+        ;;
+    esac
+}
+
 # apps
 has-cmd starship && eval "$(starship init bash)"
 has-cmd zoxide   && eval "$(zoxide init bash)"
